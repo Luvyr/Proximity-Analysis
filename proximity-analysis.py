46e43f0f6ff2d9b0ed0bab8e6ac4b77c3a44f2c5
Luvyr <Luvyr@users.noreply.github.com>
2021-03-14 00:00:14 +0100

    proximity-analysis

--- proximity-analysis.py
+++ proximity-analysis.py
@@ -7,6 +7,14 @@ import folium
 from folium import Choropleth, Marker
 from folium.plugins import HeatMap, MarkerCluster
 
+def embed_map(m, file_name):
+    from IPython.display import IFrame
+    m.save(file_name)
+    return IFrame(file_name, width='100%', height='500px')
+
+collisions = gpd.read_file("./NYPD_Motor_Vehicle_Collisions.shp")
+collisions.head()
+
 hospitals = gpd.read_file("./nyu_2451_34494.shp")
 hospitals.head()
 
